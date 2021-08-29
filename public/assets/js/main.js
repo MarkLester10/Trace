996d1914c1809bca3fee750eec3e8cb9af7a4d93
Mark Lester Morta <marklester10.mlsm@gmail.com>
2021-08-29 15:26:52 +0800

    added sticky nav

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,38 @@
-function displayImage(t){if(t.files[0]){var e=new FileReader;e.onload=function(t){document.querySelector("#imageDisplay").setAttribute("src",t.target.result)},e.readAsDataURL(t.files[0])}}const modal=document.querySelector(".modal"),tooltipPopUp=document.querySelector(".popup-tooltip"),tooltipClose=document.querySelector(".close-tooltip");function toggleModal(){modal.classList.toggle("active")}tooltipPopUp.addEventListener("click",function(t){t.preventDefault(),toggleModal()}),tooltipClose.addEventListener("click",function(t){t.preventDefault(),toggleModal()}),modal.addEventListener("click",function(t){t.target.classList.contains("modal")&&modal.classList.toggle("active")});
+
+window.onscroll = function() {stickyNav()};
+var navbar = document.getElementById("navbar");
+function stickyNav() {
+  if (window.pageYOffset >= 10) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+function displayImage(t) {
+  if (t.files[0]) {
+      var e = new FileReader();
+      (e.onload = function (t) {
+          document.querySelector("#imageDisplay").setAttribute("src", t.target.result);
+      }),
+          e.readAsDataURL(t.files[0]);
+  }
+}
+const modal = document.querySelector(".modal"),
+  tooltipPopUp = document.querySelector(".popup-tooltip"),
+  tooltipClose = document.querySelector(".close-tooltip");
+function toggleModal() {
+  modal.classList.toggle("active");
+}
+tooltipPopUp.addEventListener("click", function (t) {
+  t.preventDefault(), toggleModal();
+}),
+  tooltipClose.addEventListener("click", function (t) {
+      t.preventDefault(), toggleModal();
+  }),
+  modal.addEventListener("click", function (t) {
+      t.target.classList.contains("modal") && modal.classList.toggle("active");
+  });
+
+
+
