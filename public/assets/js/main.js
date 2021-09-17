cf3b41ab7144ea6dbee53cffeee229f88cba9ac7
Jan Mikko Malizon <malizonjanmikko@gmail.com>
2021-09-17 20:51:24 +0800

    Update profile page UI

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,38 +1,54 @@
-
-window.onscroll = function() {stickyNav()};
-var navbar = document.getElementById("navbar");
+window.onscroll = function () {
+	stickyNav();
+};
+var navbar = document.getElementById('navbar');
 function stickyNav() {
-  if (window.pageYOffset >= 10) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+	if (window.pageYOffset >= 10) {
+		navbar.classList.add('sticky');
+	} else {
+		navbar.classList.remove('sticky');
+	}
 }
 
 function displayImage(t) {
-  if (t.files[0]) {
-      var e = new FileReader();
-      (e.onload = function (t) {
-          document.querySelector("#imageDisplay").setAttribute("src", t.target.result);
-      }),
-          e.readAsDataURL(t.files[0]);
-  }
+	if (t.files[0]) {
+		var e = new FileReader();
+		(e.onload = function (t) {
+			document.querySelector('#imageDisplay').setAttribute('src', t.target.result);
+		}),
+			e.readAsDataURL(t.files[0]);
+	}
 }
-const modal = document.querySelector(".modal"),
-  tooltipPopUp = document.querySelector(".popup-tooltip"),
-  tooltipClose = document.querySelector(".close-tooltip");
+const modal = document.querySelector('.modal'),
+	vaccineModal = document.querySelector('.vaccine-modal'),
+	tooltipPopUp = document.querySelector('.popup-tooltip'),
+	tooltipClose = document.querySelector('.close-tooltip'),
+	vaccinePopUp = document.querySelector('.popup-vaccine'),
+	vaccineClose = document.querySelector('.close-vaccine');
+
 function toggleModal() {
-  modal.classList.toggle("active");
+	modal.classList.toggle('active');
 }
-tooltipPopUp.addEventListener("click", function (t) {
-  t.preventDefault(), toggleModal();
+tooltipPopUp.addEventListener('click', function (t) {
+	t.preventDefault(), toggleModal();
 }),
-  tooltipClose.addEventListener("click", function (t) {
-      t.preventDefault(), toggleModal();
-  }),
-  modal.addEventListener("click", function (t) {
-      t.target.classList.contains("modal") && modal.classList.toggle("active");
-  });
+	tooltipClose.addEventListener('click', function (t) {
+		t.preventDefault(), toggleModal();
+	}),
+	modal.addEventListener('click', function (t) {
+		t.target.classList.contains('modal') && modal.classList.toggle('active');
+	});
 
-
-
+// Vaccine Popup
+function toggleVaccineModal() {
+	vaccineModal.classList.toggle('active');
+}
+vaccinePopUp.addEventListener('click', function (t) {
+	t.preventDefault(), toggleVaccineModal();
+}),
+	vaccineClose.addEventListener('click', function (t) {
+		t.preventDefault(), toggleVaccineModal();
+	}),
+	vaccineModal.addEventListener('click', function (t) {
+		t.target.classList.contains('vaccine-modal') && vaccineModal.classList.toggle('active');
+	});
