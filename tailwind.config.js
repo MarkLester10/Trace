64bf593fb98db1aa4f1fe69f373127fb6fe60ba1
Jan Mikko Malizon <malizonjanmikko@gmail.com>
2021-10-05 13:56:31 +0800

    added peer profile UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 module.exports = {
-  purge: {
-    // enabled: true,
-    // content: ["./public/**/*.html"],
-  },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+	purge: {
+		// enabled: true,
+		// content: ["./public/**/*.html"],
+	},
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				blue: {
+					light: '#EFF6FF',
+				},
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [require('@tailwindcss/forms')],
 };
